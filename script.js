d1ea519709ad4439c84cee7fbb2a2a8090d47e98
laxmis-prog <laxmisanuu5@gmail.com>
2026-01-10 10:46:49 +0200

    Complete the getRandomComputerResult function so that it returns a random option from the options array.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function getRandomComputerResult() {
   const options = ["Rock", "Paper", "Scissors"];
-
+  return options [Math.floor(Math.random() * 3)];
 }
 console.log(getRandomComputerResult());
