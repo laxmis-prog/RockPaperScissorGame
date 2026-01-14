d1e925e96b45bd93a11a23f4d96fa251070f4921
laxmis-prog <laxmisanuu5@gmail.com>
2026-01-14 10:34:57 +0200

    Complete the hasPlayerWonTheRound function.This function has two parameters: player and computer.The function should return true if the player has won the round,and false if the player has lost or tied the round.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-
 function getRandomComputerResult() {
   const options = ["Rock", "Paper", "Scissors"];
-  return options [Math.floor(Math.random() * 3)];
+  return options[Math.floor(Math.random() * 3)];
 }
-console.log(getRandomComputerResult());
+
+function hasPlayerWonTheRound(player, computer) {
+  return (
+    (player === "Rock" && computer === "Scissors") ||
+    (player === "Scissors" && computer === "Paper") ||
+    (player === "Paper" && computer === "Rock")
+  );
+}
+
+console.log(hasPlayerWonTheRound("Rock", "Scissors"));
+console.log(hasPlayerWonTheRound("Scissors", "Rock"));
